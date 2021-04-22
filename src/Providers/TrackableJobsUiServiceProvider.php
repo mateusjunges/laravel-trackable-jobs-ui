cc4190d6c15affeb4ecb99aa8f6f2277e066a750
mateusjunges <mateus@junges.dev>
2021-04-22 16:22:15 -0300

    wip

--- src/Providers/TrackableJobsUiServiceProvider.php
+++ src/Providers/TrackableJobsUiServiceProvider.php
@@ -2,17 +2,27 @@
 
 namespace Junges\TrackableJobsUi\Providers;
 
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\View\Compilers\BladeCompiler;
+use Junges\TrackableJobsUi\Http\Livewire\JobStatus;
+use Livewire\Livewire;
 
 class TrackableJobsUiServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        //
+        $this->app->afterResolving(BladeCompiler::class, function () {
+            if (class_exists(Livewire::class)) {
+                Livewire::component('job-status', JobStatus::class);
+            }
+        });
     }
 
     public function boot()
     {
+        $this->registerComponents();
+
         $this->publishes([
             __DIR__."/../../config/trackable-jobs-ui.php" => config_path('trackable-jobs-ui.php')
         ], 'trackabel-jobs-ui-config');
@@ -27,4 +37,23 @@ class TrackableJobsUiServiceProvider extends ServiceProvider
             __DIR__."/../../resources/views" => resource_path('views/vendor/junges/trackable-jobs')
         ], 'trackable-jobs-views');
     }
+
+    public function registerComponents()
+    {
+        $this->callAfterResolving(BladeCompiler::class, function () {
+            $this->registerComponent('job-status');
+        });
+    }
+
+    /**
+     * Register the given component.
+     *
+     * @param  string  $component
+     * @return void
+     */
+    protected function registerComponent($component)
+    {
+        Blade::component('jetstream::components.'.$component, 'jet-'.$component);
+    }
+
 }
