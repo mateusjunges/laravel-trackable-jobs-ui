ece7254984a75b7f6e3375f379707c887f7e221a
mateusjunges <mateus@junges.dev>
2021-04-22 17:33:26 -0300

    wip

--- resources/views/components/job-status.blade.php
+++ resources/views/components/job-status.blade.php
@@ -1,15 +1,16 @@
 <div>
+    <link href="{{ asset('public/vendor/trackable-jobs-ui/css/app.css') }}" rel="stylesheet">
     <div class="bg-white">
         <div class="p-4 border-b">
             <div class="flex items-center mb-1">
                 <h1 class="text-xl mr-4">Tracked Jobs</h1>
             </div>
-{{--            <p class="text-sm text-gray-600">--}}
-{{--                Deployed to <b>{{ $deployment->environment->name }}</b> by--}}
-{{--                <b>{{ $deployment->initiator->name }}</b>--}}
+            {{--            <p class="text-sm text-gray-600">--}}
+            {{--                Deployed to <b>{{ $deployment->environment->name }}</b> by--}}
+            {{--                <b>{{ $deployment->initiator->name }}</b>--}}
             </p>
         </div>
-        @foreach ($trackedJobs as $job)
+    @foreach ($trackedJobs as $job)
             <div class="border-b">
                 <div class="flex justify-between items-center p-2 px-4">
                     <div class="flex items-center">
@@ -27,4 +28,4 @@
         @endforeach
     </div>
 </div>
-<script src="{{ asset('public/vendor/trackable-jobs-ui/css/app.css') }}"></script>
+
