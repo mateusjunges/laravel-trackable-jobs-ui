c42d59e2213a4180c7b655439b82ae0a3e94cd4e
mateusjunges <mateus@junges.dev>
2021-04-22 16:53:09 -0300

    wip

--- resources/views/components/job-status.blade.php
+++ resources/views/components/job-status.blade.php
@@ -2,26 +2,25 @@
     <div class="bg-white">
         <div class="p-4 border-b">
             <div class="flex items-center mb-1">
-                <h1 class="text-xl mr-4">{{ $deployment->commit_message }}</h1>
-                <x-trackable-jobs-status :status="$deployment->status" />
+                <h1 class="text-xl mr-4">Tracked Jobs</h1>
             </div>
-            <p class="text-sm text-gray-600">
-                Deployed to <b>{{ $deployment->environment->name }}</b> by
-                <b>{{ $deployment->initiator->name }}</b>
+{{--            <p class="text-sm text-gray-600">--}}
+{{--                Deployed to <b>{{ $deployment->environment->name }}</b> by--}}
+{{--                <b>{{ $deployment->initiator->name }}</b>--}}
             </p>
         </div>
-        @foreach ($deployment->steps as $step)
+        @foreach ($trackedJobs as $job)
             <div class="border-b">
                 <div class="flex justify-between items-center p-2 px-4">
                     <div class="flex items-center">
-                        <span class="text-gray-700 text-sm">{{ $step->label() }}</span>
+                        <span class="text-gray-700 text-sm"># {{ $job->id }}</span>
                     </div>
 
                     <div class="flex items-center">
                         <span class="text-sm mr-2 text-gray-600">
-                            {{ $step->duration() }}
+{{--                            {{ $job->duration() }}--}}Test duration
                         </span>
-                        <x-trackable-jobs-status :status="$step->status" />
+                        <x-trackable-jobs-status :status="$job->status" />
                     </div>
                 </div>
             </div>
